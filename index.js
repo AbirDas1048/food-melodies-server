c41b49c8d4823287bc942c231383fbfa0e57d0ed
Abir Das <abirdas422@gmail.com>
2022-11-10 01:39:32 +0600

    Review added

--- index.js
+++ index.js
@@ -27,25 +27,25 @@ async function run() {
         // let isoDate = new Date(date);
         // console.log(date, isoDate);
 
-        app.get('/services', async(req, res) => {
+        app.get('/services', async (req, res) => {
             const limit = parseInt(req.query.limit);
             //const size = parseInt(req.query.size);
             //console.log(limit);
             const query = {};
-            let services =[];
+            let services = [];
             const options = {
                 // sort matched documents in descending order by rating
                 sort: { "createdAt": -1 },
                 // Include only the `title` and `imdb` fields in the returned document
                 //projection: { _id: 0, title: 1, imdb: 1 },
-              };
+            };
             const cursor = serviceCollection.find(query, options);
-            if(limit){
+            if (limit) {
                 services = await cursor.skip(0).limit(limit).toArray();
-            }else{
+            } else {
                 services = await cursor.toArray();
             }
-            
+
             // const count = await productCollection.estimatedDocumentCount();
             res.send(services);
 
@@ -58,26 +58,38 @@ async function run() {
             let createdAt = new Date(date);
             let updatedAt = new Date(date);
 
-            const service = {...data, createdAt, updatedAt }
+            const service = { ...data, createdAt, updatedAt }
             //console.log(service);
             const result = await serviceCollection.insertOne(service);
             res.send(result);
         });
 
+        app.get('/service/:id', async (req, res) => {
+
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            const details = await serviceCollection.findOne(query);
+            res.send(details);
+
+        });
+
         app.post('/review', async (req, res) => {
             const data = req.body;
+
+            //const ratings = parseFloat(data.ratings);
 
             let date = new Date().toISOString();
             let createdAt = new Date(date);
             let updatedAt = new Date(date);
 
-            const review = {...data, createdAt, updatedAt }
+            const review = { ...data, createdAt, updatedAt }
             console.log(review);
-            //const result = await reviewCollection.insertOne(review);
-            //res.send(result);
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         });
 
-        
+
     } finally {
         //await client.close(); 
     }
